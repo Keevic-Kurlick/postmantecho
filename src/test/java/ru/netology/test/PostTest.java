cfa57d745f7b2c6fdf256728e280f85913f6c327
Keevic-Kurlick <tyan.yumiko1@gmail.com>
2022-05-15 17:28:20 +0300

    test on post

---
+++ src/test/java/ru/netology/test/PostTest.java
@@ -0,0 +1,24 @@
+package ru.netology.test;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostTest {
+    @Test
+    void shouldPost() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("johnatan")
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("johnatan"));
+    }
+}
